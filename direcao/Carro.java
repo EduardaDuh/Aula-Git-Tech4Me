7b90d18992577989df1ab944ee86079fbb971ede
EduardaDuh <eduuardarocha12@gmail.com>
2021-11-17 21:43:51 -0300

    Meu primeiro Commit

---
+++ direcao/Carro.java
@@ -0,0 +1,12 @@
+package direcao;
+
+public class Carro {
+    private int velocidade;
+
+    public void  acelerar (int velocidade){
+        this.velocidade
+    }
+    public int getVelocidade(){
+        return this.velocidade;
+    }
+}
